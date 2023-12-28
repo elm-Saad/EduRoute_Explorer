1f1b709ebb72bfd223399137811a9244ca020211
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-12-28 14:48:18 +0100

    update

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -3,19 +3,19 @@ import React, { useState } from 'react'
 import BarChart from './BarChart'
 import AreaCharts from './AreaCharts'
 import { useSelector } from 'react-redux'
-import RadialBarCharts  from './RadialBarChart'
+import LineCharts  from './LineCharts'
 
 
 
 const ChartsContainer = ()=>{
     const { monthlyApplications: data } = useSelector((store) => store.allCourses)
-    console.log(data)
+   
     return <section className='my-10 text-center'>
       <h4 className='text-xl font-semibold'>Monthly Courses</h4>
       <section className='my-10 grid grid-cols-1 md:grid-cols-2 md:gap-4'>
-        <BarChart data={data} />
-        <AreaCharts data={data} />
-        <RadialBarCharts data={data}/>
+          <BarChart data={data} />
+          <LineCharts data={data}/>
+          <AreaCharts data={data} />
       </section>
     </section>
     
